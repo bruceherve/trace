f30082590e6a5d4a99f07f69543813ef614aac4e
hernino <hernino25@gmail.com>
2018-05-14 16:29:16 +0200

    added trace package

---
+++ tracer.go
@@ -0,0 +1,27 @@
+package trace
+
+import (
+	"fmt"
+	"io"
+)
+
+// Tracer is the interface that describes an object capable of
+// tracing events throughout code
+type Tracer interface {
+	Trace(...interface{})
+}
+
+//tracer holds the trace output
+type tracer struct {
+	out io.Writer
+}
+
+func (t *tracer) Trace(a ...interface{}) {
+	t.out.Write([]byte(fmt.Sprint(a...)))
+	t.out.Write([]byte("\n"))
+}
+
+//New is place holder for now
+func New(w io.Writer) Tracer {
+	return &tracer{out: w}
+}
